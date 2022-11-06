5114336a2c5d636e2f8805ae60343dc63fd32adc
Frank C <>
2022-11-06 22:28:30 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,85 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// bool isUnique(string s) {
+//   string aux1;
+//   string aux2;
+//   for (int i = 0; i < s.size(); i++) {
+//     aux1 = s[i];
+//     for (int j = i + 1; j < s.size(); j++) {
+//       aux2 = s[j];
+//       if (aux1 == aux2) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// }
+
+// void pruebaTodosLosCaracteresSonUnicos(string s) {
+//   if (isUnique(s)) {
+//     cout << "Todos los caracteres son unicos" << endl;
+//   } else {
+//     cout << "Un caracter se repite" << endl;
+//   }
+// }
+
+// int main() {
+
+//   string s = "aaa";
+
+//   pruebaTodosLosCaracteresSonUnicos("abcde");
+//   pruebaTodosLosCaracteresSonUnicos("abcdea");
+//   pruebaTodosLosCaracteresSonUnicos("aaa");
+//   pruebaTodosLosCaracteresSonUnicos("aabbaa");
+// }
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+// bool isUnique(string s) {
+
+//   vector<char> lista = {};
+
+//   for (int i = 0; i < s.size(); i++) {
+//     if(find(lista.begin(), lista.end(), s[i])){
+//       lista.push_back(s[i]);
+//     }else{
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// void pruebaTodosLosCaracteresSonUnicos(string s) {
+//   if (isUnique(s)) {
+//     cout << "Todos los caracteres son unicos" << endl;
+//   } else {
+//     cout << "Un caracter se repite" << endl;
+//   }
+// }
+
+int main() {
+
+  // string s = "aaa";
+
+	std::vector<int> v;
+
+	v.
+
+  // pruebaTodosLosCaracteresSonUnicos("abcde");
+  // pruebaTodosLosCaracteresSonUnicos("abcdea");
+  // pruebaTodosLosCaracteresSonUnicos("aaa");
+  // pruebaTodosLosCaracteresSonUnicos("aabbaa");
+}
+
+
+
+
